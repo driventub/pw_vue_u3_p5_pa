ae5d91da89b9018d7c690588a300bd7dc856e851
driven <aldazpaulo@gmail.com>
2024-02-14 23:03:04 -0500

    css y mas campos

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -1,14 +1,35 @@
 <template>
     <div class="container">
-        <div class="formEstudiantes"></div>
-        <h1>Consulta por Estudiantes</h1>
-        <input v-model="id" type="number" />
-        <label for="">{{ nombre }}</label>
-        <label for="">{{ apellido }}</label>
-        <button @click="consultarPorId">Consultar</button>
+        <div class="formInsertar">
+            <h1>Consulta por Estudiantes</h1>
+            <input v-model="id" type="number" />
+            <button @click="consultarPorId">Consultar</button>
 
-        <input v-model="apellido" type="text" />
-        <button @click="insertar">Insertar</button>
+        </div>
+    </div>
+
+    <div class="container">
+
+
+
+        <div class="formInsertar">
+            <h1>Inserción de Estudiantes</h1>
+
+            <label for="nombre">Nombre</label>
+            <input id="nombre" v-model="nombre" type="text" />
+            <label for="apellido">Apellido</label>
+            <input id="apellido" v-model="apellido" type="text" />
+            <label for="genero">Genero</label>
+            <input id="genero" v-model="genero" type="text" />
+            <label for="fecha">Fecha de Nacimiento</label>
+            <input id="fecha" v-model="fechaNacimiento" type="datetime-local" />
+            <label for="tel">Numero de Telefono</label>
+            <input id="tel" v-model="telefono" type="text" />
+
+            <button @click="insertar">Insertar</button>
+        </div>
+
+
 
 
     </div>
@@ -22,8 +43,11 @@ export default {
     data() {
         return {
             id: null,
-            nombre: "Nombre",
-            apellido: "Apellido"
+            nombre: "",
+            apellido: "",
+            genero: "",
+            fechaNacimiento: "",
+            telefono: "",
 
         }
     },
@@ -44,15 +68,15 @@ export default {
             console.log("Insertando....");
             const datos = {
 
-                "nombre": "Vue",
+                "nombre": this.nombre,
                 "apellido": this.apellido,
-                "genero": "M",
-                "fechaNacimiento": "1999-01-01T00:00:00",
+                "genero": this.genero,
+                "fechaNacimiento": this.fechaNacimiento,
                 "semestre": "tercero",
                 "afiliadoSeguro": false,
                 "gratuidad": true,
                 "nivelEconomico": "alto",
-                "numTelefono": "09872362919",
+                "numTelefono": this.telefono,
                 "domicilio": "Quito"
             }
 
@@ -66,8 +90,25 @@ export default {
 
 <style scoped>
 .container {
-    display: grid;
+    display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+}
+
+.formInsertar {
+    margin-top: 2rem;
+    display: grid;
+    padding: 1rem;
+    background-color: #dda15e;
+    border-radius: 2rem;
+
+}
+
+label {
+    text-align: left;
+}
+
+input {
+    background-color: #fefae0;
 }
 </style>
